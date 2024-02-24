b8f7e813e1f07673c8d35d180daef70e04adfda8
fmata97 <fabiomata97@gmail.com>
2024-02-24 20:51:26 +0000

    feat: client debug messages

--- Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
+++ Client/src/main/java/pt/ulisboa/tecnico/tuplespaces/client/grpc/ClientService.java
@@ -6,6 +6,7 @@ import io.grpc.StatusRuntimeException;
 import java.util.List;
 import pt.ulisboa.tecnico.tuplespaces.centralized.contract.TupleSpacesCentralized.*;
 import pt.ulisboa.tecnico.tuplespaces.centralized.contract.TupleSpacesGrpc;
+import pt.ulisboa.tecnico.tuplespaces.client.ClientMain;
 import pt.ulisboa.tecnico.tuplespaces.client.exceptions.ClientServiceException;
 
 public class ClientService {
@@ -15,48 +16,63 @@ public class ClientService {
 
   public ClientService(String target) {
     channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+    ClientMain.debug("channel was created successfully\n");
     stub = TupleSpacesGrpc.newBlockingStub(channel);
+    ClientMain.debug("stub was created successfully\n");
   }
 
   public void put(String newTuple) throws ClientServiceException {
     try {
-      this.stub.put(PutRequest.newBuilder().setNewTuple(newTuple).build());
+      ClientMain.debug("calling procedure put('%s')\n", newTuple);
+      PutResponse response = this.stub.put(PutRequest.newBuilder().setNewTuple(newTuple).build());
+      ClientMain.debug("response: %s\n", response);
     } catch (StatusRuntimeException e) {
+      ClientMain.debug("RPC failed: %s\n", e.getStatus());
       throw new ClientServiceException(e);
     }
   }
 
   public String read(String searchPattern) throws ClientServiceException {
     try {
-      return (this.stub
-          .read(ReadRequest.newBuilder().setSearchPattern(searchPattern).build())
-          .getResult());
+      ClientMain.debug("calling procedure read('%s')\n", searchPattern);
+      ReadResponse response =
+          this.stub.read(ReadRequest.newBuilder().setSearchPattern(searchPattern).build());
+      ClientMain.debug("response: %s", response);
+      return response.getResult();
     } catch (StatusRuntimeException e) {
+      ClientMain.debug("RPC failed: %s\n", e.getStatus());
       throw new ClientServiceException(e);
     }
   }
 
   public String take(String searchPattern) throws ClientServiceException {
     try {
-      return (this.stub
-          .take(TakeRequest.newBuilder().setSearchPattern(searchPattern).build())
-          .getResult());
+      ClientMain.debug("calling procedure take('%s')\n", searchPattern);
+      TakeResponse response =
+          this.stub.take(TakeRequest.newBuilder().setSearchPattern(searchPattern).build());
+      ClientMain.debug("response: %s", response);
+      return response.getResult();
     } catch (StatusRuntimeException e) {
+      ClientMain.debug("RPC failed: %s\n", e.getStatus());
       throw new ClientServiceException(e);
     }
   }
 
   public List<String> getTupleSpacesState() throws ClientServiceException {
     try {
-      return (this.stub
-          .getTupleSpacesState(getTupleSpacesStateRequest.getDefaultInstance())
-          .getTupleList());
+      ClientMain.debug("calling procedure getTupleSpacesState()\n");
+      getTupleSpacesStateResponse response =
+          this.stub.getTupleSpacesState(getTupleSpacesStateRequest.getDefaultInstance());
+      ClientMain.debug("response: %s", response.getTupleCount() == 0 ? "\n" : response);
+      return response.getTupleList();
     } catch (StatusRuntimeException e) {
+      ClientMain.debug("RPC failed: %s\n", e.getStatus());
       throw new ClientServiceException(e);
     }
   }
 
   public void shutdownChannel() {
     channel.shutdownNow();
+    ClientMain.debug("channel successfully shutdown\n");
   }
 }
